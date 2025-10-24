c88035c484958407eda8b16746d67438832c5c32
Ketan Tailor <ketan.tailor@live.co.uk>
2025-10-24 14:09:46 +0100

    Comments

--- src/AdventOfCode.ConsoleApp/AocPuzzleAttribute.cs
+++ src/AdventOfCode.ConsoleApp/AocPuzzleAttribute.cs
@@ -5,6 +5,9 @@ public class AocPuzzleAttribute(string name) : Attribute
 {
     public string Name { get; set; } = name;
 
+    /// <summary>The solution for part 1. Required for the verify feature.</summary>
     public string? Solution1 {  get; set; }
+
+    /// <summary>The solution for part 2. Required for the verify feature.</summary>
     public string? Solution2 {  get; set; }
 }
