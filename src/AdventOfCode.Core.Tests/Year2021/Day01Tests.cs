bd2b47a3aa2a361183972d4369165b11600ac11d
Ketan Tailor <ketan.tailor@live.co.uk>
2025-11-16 14:25:44 +0000

    AdventOfCode.Core.Tests.csproj

---
+++ src/AdventOfCode.Core.Tests/Year2021/Day01Tests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode.Core.Tests.Year2021;
+
+[TestFixture]
+public class Day01Tests
+{
+    [Test]
+    public void Test1()
+    {
+        var input = """
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+            """;
+
+        var solution = new AdventOfCode.Core.Year2021.Day01();
+        var result = solution.Solve(input);
+
+        Assert.That(result.Part1, Is.EqualTo("7"));
+        Assert.That(result.Part2, Is.EqualTo("5"));
+    }
+}
