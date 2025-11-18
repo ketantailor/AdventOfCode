f87861c3bdd647caca068d08e82ef77f40487efa
Ketan Tailor <ketan.tailor@live.co.uk>
2025-11-18 20:42:44 +0000

    2021.07

---
+++ src/AdventOfCode.Core.Tests/Year2021/Day07Tests.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode.Core.Year2021;
+
+namespace AdventOfCode.Core.Tests.Year2021;
+
+[TestFixture]
+public class Day07Tests
+{
+    [Test]
+    public void Test_Initial()
+    {
+        var input = "16,1,2,0,4,2,7,1,2,14";
+
+        var solution = new Day07();
+        var result = solution.Solve(input);
+
+        Assert.That(result.Part1, Is.EqualTo("37"));
+        Assert.That(result.Part2, Is.EqualTo("168"));
+    }
+}
---
+++ src/AdventOfCode.Core/Year2021/Day07.cs
@@ -0,0 +1,42 @@
+﻿
+namespace AdventOfCode.Core.Year2021;
+
+[AocPuzzle("The Treachery of Whales", Solution1 = "342534", Solution2 = "94004208")]
+internal class Day07 : ISolution
+{
+    public SolutionResult Solve(string input)
+    {
+        var positions = input.Split(',').Select(int.Parse).ToArray();
+
+        var part1 = Enumerable.Range(positions.Min(), positions.Max())
+            .Select(r => Score1(positions, r))
+            .Min();
+
+        var part2 = Enumerable.Range(positions.Min(), positions.Max())
+            .Select(r => Score2(positions, r))
+            .Min();
+
+        return new SolutionResult(part1, part2);
+    }
+
+    private static int Score1(int[] ints, int ave)
+    {
+        var score = ints
+            .Select(i => Math.Abs(i - ave))
+            .Sum();
+        return score;
+    }
+
+    private static int Score2(int[] ints, int ave)
+    {
+        var score = ints
+            .Select(i => Triangle(Math.Abs(i - ave)))
+            .Sum();
+        return score;
+    }
+
+    private static int Triangle(int i)
+    {
+        return i * (i + 1) / 2;
+    }
+}
